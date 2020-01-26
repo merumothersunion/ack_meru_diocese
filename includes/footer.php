643b77ef5f1b84d54189bb0943ed07942c028037
mukule <nelsonmasibo6@gmail.com>
2020-01-26 22:59:49 +0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,53 @@
+<div class="fixed-bottom">
+	<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> CUEH <div class="content-grids">
+		    	<div class="wrap">
+		    	<div class="section group">
+								
+							
+				<div class="listview_1_of_3 images_1_of_3">
+					<div class="listimg listimg_1_of_2">
+						  <img src="images/1.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3>Patients</h3>
+						  <p>Register & Book Appointment</p>
+						  <div class="button"><span><a href="hms/user-login.php">Click Here</a></span></div>
+				    </div>
+				</div>	
+
+				<div class="listview_1_of_3 images_1_of_3">
+					<div class="listimg listimg_1_of_2">
+						  <img src="images/8.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3>Doctors Login</h3>
+						
+						  <div class="button"><span><a href="hms/doctor/">Click Here</a></span></div>
+					</div>
+				</div>
+
+
+					<div class="listview_1_of_3 images_1_of_3">
+					<div class="listimg listimg_1_of_2">
+						  <img src="images/grid-img1.png">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3>Admin Login</h3>
+						
+						  <div class="button"><span><a href="hms/admin">Click Here</a></span></div>
+				     </div>
+				</div>			
+			</div>
+		    </div>
+		   </div>
+		   
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
+			</div>
---
+++ nav.php
@@ -0,0 +1,28 @@
+ <div class="fixed-top">
+   <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+ <!-- <div class="header">-->
+        
+        <!--start-logo-->
+        
+          <img src="images/logo\.jpg" width="7%" height="7%">  <a class="navbar-brand" href="index.php"><h3>&nbsp;Chuka University E-Hospital<br/>&nbsp;<small><i>(Sapientia Divitia Est)</i></small></h3></a>
+        <!--end-logo-->
+        <!--start-top-nav-->
+        <div class="wrap">
+        <div class="top-nav">
+          <ul>
+            <li class="active"><a href="index.php">Home</a></li>
+            
+            <li><a href="contact.php">contact</a></li>
+          </ul>         
+        </div>
+        <div class="clear"> </div>
+      </div></nav>
+         <nav class="navbar navbar-expand-lg navbar-light bg-warning"></nav>
+        <!--end-top-nav-->
+      </div>
+      <!--end-header-->
+
+    </div>
+
+    
+    
